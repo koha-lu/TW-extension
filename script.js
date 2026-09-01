bfb1ebfa5dc392a68cb42a478cb63bc38f60faa4
koha_lu <h.r.t.ebitake925@gmail.com>
2026-09-01 14:27:59 +0000

    changed script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext();
+
+ctx.fillStyle = "rgb(200, 0, 0)";
+ctx.fillRect(10, 10, 50, 50);
+
+ctx.fillStyle = "rgb(0 0 200 / 50%)";
+ctx.fillRect(30, 30, 50, 50);
