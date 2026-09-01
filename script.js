f0cb6525a2e34d769580ae3ba33ffa14c31a75a1
koha_lu <h.r.t.ebitake925@gmail.com>
2026-09-01 14:43:14 +0000

    add files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("my-canvas");
-const ctx = canvas.getContext();
+const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "rgb(200, 0, 0)";
 ctx.fillRect(10, 10, 50, 50);
